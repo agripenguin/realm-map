9567cb8a2aa7d631401a3e4d056c8a12b3029f8f
agripenguin <0818y.naoki@gmail.com>
2020-10-11 01:45:24 +0900

    統合地図ダウンロード機能の追加

--- app.py
+++ app.py
@@ -1,36 +1,31 @@
-from flask import Flask, request, redirect
-from flask import send_file
+from flask import Flask, request, redirect, render_template, session
+from flask import send_file, Markup
 from datetime import date
 import os
+import re
 import magic
 import detect
+import integrate
 
 IMAGES_DIR = './static/images'
 IMAGES_URL = '/static/images'
 
 app = Flask(__name__)
+app.secret_key = '3Kj?2Yd['
 
 #データサイズの制限:4MB
 app.config['MAX_CONTENT_LENGTH'] = 2 * 1024 * 1024 
 
 @app.route('/')
 def index_page():
-    return """
-        <html><body><h1>アップロード</h1>
-        <form action="/upload"
-            method="POST"
-            enctype="multipart/form-data">
-            <input type="file" name="upfile"></br>
-            拡張段階：<input type="number" min="0" max="4" name="zoom_level" value="0"></br>
-            x：       <input type="number" name="x"></br>
-            y：       <input type="number" name="y"></br>
-            <input type="submit" value="アップロード">
-        </form>
-        </body></html>
-    """
+    return render_template('index.html')
 
-@app.route('/upload', methods=['POST'])
-def upload():
+@app.route('/upload')
+def upload_from_mobile():
+    return render_template('upload_from_mobile.html')
+
+@app.route('/upload_go', methods=['POST'])
+def upload_go():
     if not ('upfile' in request.files) or not (('zoom_level' or 'x' or 'y') in request.form):
         return redirect('/')
 
@@ -40,28 +35,55 @@ def upload():
     temp_y = request.form['y']
     if temp_file.filename == '' or temp_zoom == '' or temp_x == '' or temp_y == '':
         return redirect('/')
-
     fp = temp_file.stream
-    mime = magic.from_buffer(fp.read(1024), mime=True) 
-    fp.seek(0)
-    #from_buffer()の引数はファイルオブジェクトをreadしたもの。openの場合、open('*', 'rb')でバイナリとしてreadする。
-    
-    if mime != 'image/png' and mime != 'image/jpeg':
-    #if not is_jpegfile(temp_file.stream):
-        return '<h1>画像ファイル以外アップできません</h1>'
-    
     dt_now = str(date.today())
-    save_dir = IMAGES_DIR + '/' + dt_now
+    mime = check_mime(fp)
     fname = temp_zoom + ',' + temp_x + ',' + temp_y + '.' + mime.split('/')[1]
-
-    try:
-        os.makedirs(save_dir, exist_ok=True)
-    except FileExistsError:
-        pass
-
-    detect.detect_map(fp).save(save_dir +'/'+fname)
-    #temp_file.save(IMAGES_DIR+'/'+fname)
+    
+    if check_save_file(fp, fname, dt_now) == -1:
+        return '<h1>画像ファイル以外アップできません</h1>'
     return redirect('/photo/'+dt_now+'_'+fname)
+
+@app.route('/multi_upload')
+def multi():
+    return render_template('multi_upload.html')
+
+@app.route('/multi_upload_go', methods=['POST'])
+def multi_go():
+    flag = 0
+    if not ('upload_files' in request.files):
+        return redirect('/')
+
+    upload_files = request.files.getlist('upload_files')
+    
+    if upload_files == []:
+        return redirect('/')
+    for upload_file in upload_files:
+        fp = upload_file.stream
+        dt_now = str(date.today())
+        fname = upload_file.filename
+        upflag = check_save_file(fp, fname, dt_now)
+        if upflag == -1:
+            session[fname] = '画像ファイル以外アップできません'
+        elif upflag == -2:
+            session[fname] = 'ファイル名が正しくありません'
+        else:
+            session[fname] = 'ok'
+        flag += upflag
+    session['flag'] = flag
+    return redirect('/multi_finish')
+    #redirect('/photo/'+dt_now+'_'+fname)
+@app.route('/multi_finish')
+def finish():
+    allFiles = ''
+    if session['flag'] == 0:
+        title = 'アップロードが正常に完了しました'
+    else:
+        title = 'アップロードに問題がある可能性があります'
+    for fname in session.keys():
+        if type(session[fname]) == str:
+            allFiles = allFiles + fname + ': ' + session[fname] + Markup('<br>')
+    return render_template('multi_completed.html', title = title, pass_fail = allFiles)
 
 @app.route('/photo/<dir_fname>')
 def photo_page(dir_fname):
@@ -71,16 +93,7 @@ def photo_page(dir_fname):
     image_url = IMAGES_URL + '/' + dt + '/' + fname
     if not os.path.exists(image_path):
         return '<h1>画像がありません</h1>'
-    return """
-        <h1>画像がアップロードされました</h1>
-        <p>URL: {0}<br>
-        file: {1}</p>
-        <img src="{0}" width="400">
-        <p>
-        <a href="/download/{2}">ダウンロード</a>
-        <a href="/">他のファイルをアップロード</a>
-        </p>
-    """.format(image_url, image_path, dir_fname)
+    return render_template('completed.html', url=image_url, path=image_path, dir_fname=dir_fname)
 
 @app.route('/download/<dir_fname>')
 def download(dir_fname):
@@ -88,12 +101,44 @@ def download(dir_fname):
     dt, fname = dir_fname.split('_')
     download_file = IMAGES_DIR + '/' + dt + '/' + fname
     return send_file(download_file, as_attachment =True, attachment_filename=fname)
-    
 
-def is_jpegfile(fp):
-    byte = fp.read(2)
+@app.route('/integrate')
+def request_integrate():
+    return render_template('request_integrate.html')
+
+@app.route('/integrate_download', methods=['POST'])
+def integrate_download():
+    area = request.form['area']
+    dirname = integrate.integrate_go(area)
+    return send_file(dirname, as_attachment =True)
+    #os.remove(dirname)
+
+def check_mime(fp):
+    #from_buffer()の引数はファイルオブジェクトをreadしたもの。
+    #openの場合、open('*', 'rb')でバイナリとしてreadする。
+    mime = magic.from_buffer(fp.read(1024), mime=True)
     fp.seek(0)
-    return byte[:2] == b'\xFF\xD8'
+    return mime
+
+def is_image(fp):
+    mime = check_mime(fp)
+    return mime == 'image/png' or mime == 'image/jpeg'
+
+#画像ファイルかどうかチェックした上で保存する
+def check_save_file(fp, fname, dt_now):
+    if re.fullmatch(r'\d,\d,\d.[a-z]+', fname) == None:
+        return -2
+    if not is_image(fp):
+        return -1
+    
+    save_dir = IMAGES_DIR + '/' + dt_now
+    try:
+        os.makedirs(save_dir, exist_ok=True)
+    except FileExistsError:
+        pass
+
+    detect.detect_map(fp).save(save_dir +'/'+fname)
+    return 0
 
 if __name__ == '__main__':
     app.run()
